45be67c9f3506e5784c34e3bad1980db794bb374
timedarknow <123705185+timedarknow@users.noreply.github.com>
2023-04-01 22:58:10 -0400

    Add files via upload

---
+++ die.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+userchoice1 = input( "die:" )
+
+if (userchoice1 == "ok") | (userchoice1 == "yes"):
+  print("damn bro you okay?")
+  sleep(10)   
+elif (userchoice1 == "no") | (userchoice1 == "why"):
+  print("why?")
+  sleep(10)
